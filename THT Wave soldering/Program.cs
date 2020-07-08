646af159be372c2b2d8849c3ddeae5d7c6e7df6f
volodin1971 <volodin1971@mail.ru>
2020-07-08 16:01:51 +0200

    Добавьте файлы проекта.

---
+++ THT Wave soldering/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace THT_Wave_soldering
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
